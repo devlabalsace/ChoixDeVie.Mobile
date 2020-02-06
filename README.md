80fb2eede398981999bd0e15378ca6f470534af5
Hervé PHILIPPE <alchiweb@live.fr>
2020-02-06 21:05:39 +0100

    First alpha version

---
+++ README.md
@@ -0,0 +1,11 @@
+# mobile/tablet app from the [LifeChoices project](https://github.com/devlabalsace/LifeChoices) (Xamarin 4.4+)
+
+## First version: based on Xappy from David Ortineau
+
+We are working on a new gallery app for all of us to enjoy. The app should:
+
+* Be A Collaboration - join us as we iterate through the design, user experience, and implementation.
+* Be Gorgeous - Xamarin and Xamarin.Forms are being used every day to create top notch apps. Let's show that!
+* Be Modern - Xamarin.Forms is shipping new features to make it easier than ever to deliver current user experience.
+* Be Useful - Rather than just a static gallery, the app should be useful as a tool for your to keep up to date with the steady stream of happenings in our open source community.
+* Be Ready - At Build 2019!
---
+++ src/LifeChoices.App.Android/Renderers/PressableViewRenderer.cs
@@ -0,0 +1,36 @@
+﻿using Android.Content;
+using Android.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using LifeChoices.App.Content.Scenarios.OtherLogin.Controls;
+using LifeChoices.App.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(PressableView), typeof(PressableViewRenderer))]
+namespace LifeChoices.App.Droid.Renderers
+{
+    public class PressableViewRenderer : VisualElementRenderer<PressableView>
+    {
+        public PressableViewRenderer(Context context) : base(context)
+        {
+            Touch += Control_Touch;
+        }
+
+        private void Control_Touch(object sender, TouchEventArgs e)
+        {
+            switch (e.Event.Action)
+            {
+                case MotionEventActions.Down:
+                    Element?.RaisePressed();
+                    break;
+
+                case MotionEventActions.Up:
+                    Element?.RaiseReleased();
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+    }
+}
