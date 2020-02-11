e0148d4c62b50d64626654a3b1b4832685dcbd44
Hervé PHILIPPE <alchiweb@live.fr>
2020-02-11 10:49:59 +0100

    Add ApiConstants.cs file

---
+++ src/LifeChoices.App/ApiConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace LifeChoices.App
+{
+    public static class ApiConstants
+    {
+        //TODO: Add Api Keys
+        public static string GoogleClientId = "";
+
+        //TODO: Add Api Keys
+        public static string GoogleClientSecret = "";
+
+        //TODO: Add Api Keys
+        public const string GoogleMapsKey = "";
+    }
+}
